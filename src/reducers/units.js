12567e36f6231295d3b07a02aacfaf4fd5f8d318
Burak Özel <burakk.oozel@gmail.com>
2022-07-19 11:05:44 +0300

    filtered ages in reducer

--- src/reducers/units.js
+++ src/reducers/units.js
@@ -1,18 +1,29 @@
 
 const initialState = {
     units: [],
+    status: "idle",
     loading: false,
-    error: null
+    error: null,
 }
 
 const units = ( state = initialState, action) => {
     switch(action.type) {
         case "GET_UNITS_REQUESTED":
-            return { ...state, loading: true };
+            return { ...state, loading: true, status: 'loading' };
         case "GET_UNITS_SUCCESS":
-            return { ...state, loading: false, units: action.units};
+            return {
+                ...state,
+                loading: false,
+                units: action.units,
+                status: 'success',
+                filterByDarkAge: action.units.filter(item => item.age === 'Dark'),
+                filterByFeudalAge: action.units.filter(item => item.age === 'Feudal'),
+                filterByCastleAge: action.units.filter(item => item.age === 'Castle'),
+                filterByImperialAge: action.units.filter(item => item.age === 'Imperial'),     
+                //filterByWood: action.units.filter(item => item.cost.Wood),     
+            };
         case "GET_UNITS_FAILED":
-            return { ...state, loading: false, error: action.message};
+            return { ...state, loading: false, error: action.message, status: 'failed' };
         default:
             return state;
     }
